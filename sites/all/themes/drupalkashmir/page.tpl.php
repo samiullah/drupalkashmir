5fce12cf9597923d339a47d600b05df8349514b3
iposh <iposh@iposh.com>
2013-06-05 11:27:00 +0530

    remove default title

--- sites/all/themes/drupalkashmir/page.tpl.php
+++ sites/all/themes/drupalkashmir/page.tpl.php
@@ -39,11 +39,7 @@
 <div class="page">
 
   <div role="main" id="#main-content">
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <h1 class="default_title"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
+
 
 
 
